888b033de86e958efa70d6beed99db494f596cc9
Michael Ku Yu <mikeyu.ucsd.edu@gmail.com>
2017-05-09 00:20:33 -0700

    updated align service

--- align_hierarchies.py
+++ align_hierarchies.py
@@ -1,26 +1,32 @@
 import sys, shutil, os, argparse, tempfile
 from subprocess import Popen, PIPE, STDOUT
 import tempfile
+from utilities import time_print
 
-def align_hierarchies(hier1, hier2, output, iterations, threads, calculateFDRs_cmd='/cellar/users/mikeyu/alignOntology/calculateFDRs'):
+def align_hierarchies(hier1, hier2,
+                      output,
+                      iterations, threads,
+                      calculateFDRs_cmd='/cellar/users/mikeyu/alignOntology/calculateFDRs'):
 
     if not isinstance(hier1, (str, unicode)):        
         # Write to file
-        with tempfile.NamedTemporaryFile('w', delete=False) as f:
+        with open('/tmp/tmp1.txt', 'w') as f:
+#        with tempfile.NamedTemporaryFile('w', delete=False) as f:
             f.write('\n'.join(['\t'.join([str(x[0]),str(x[1]),str(x[2])]) for x in hier1]) + '\n')
         hier1 = f.name
     else:
         assert os.path.exists(hier1)
 
     if not isinstance(hier2, (str, unicode)):
-        with tempfile.NamedTemporaryFile('w', delete=False) as g:
+        with open('/tmp/tmp2.txt', 'w') as g:
+#        with tempfile.NamedTemporaryFile('w', delete=False) as g:
             g.write('\n'.join(['\t'.join([str(x[0]),str(x[1]),str(x[2])]) for x in hier2]) + '\n')
         hier2 = g.name
     else:
         assert os.path.exists(hier2)
 
     output_dir = tempfile.mkdtemp(prefix='tmp')
-    
+
     cmd = '{5} {0} {1} 0.05 criss_cross {2} {3} {4} gene'.format(
            hier1, hier2, output_dir, iterations, threads, calculateFDRs_cmd)
     time_print(cmd)
@@ -36,14 +42,19 @@ def align_hierarchies(hier1, hier2, output, iterations, threads, calculateFDRs_c
         # 4) FDR
         # 5) Size of the term in the first ontology
         shutil.copy(os.path.join(output_dir, 'alignments_FDR_0.1_t_0.1'), output)
-
-        if os.path.isdir(output + '_dir'):
-            shutil.rmtree(output + '_dir')
-        shutil.move(output_dir, output + '_dir')
     finally:
+        # if os.path.isdir(output + '_dir'):
+        #     shutil.rmtree(output + '_dir')
+#        shutil.move(output_dir, output + '_dir')
+
+        if os.path.isdir(output_dir):
+            shutil.rmtree(output_dir)
+        
         if p.poll() is None:
             if verbose: time_print('Killing alignment process %s. Output: %s' % (p.pid, output))
             p.kill()  # Kill the process
+
+    return
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser('Align two hierarchies')
--- align_service.py
+++ align_service.py
@@ -22,8 +22,8 @@ import pandas as pd
 
 from ndex.networkn import NdexGraph
 
-from clixo import run_clixo
-from Ontology import Ontology, networkx_to_NdexGraph
+from align_hierarchies import align_hierarchies
+from Ontology import Ontology, networkx_to_NdexGraph, NdexGraph_to_Ontology
 from utilities import pivot_2_square
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
@@ -32,52 +32,133 @@ class CyServiceServicer(cx_pb2_grpc.CyServiceServicer):
 
     def StreamElements(self, element_iterator, context):
         try:
+            assert 'ALIGN_ONTOLOGY' in os.environ
+
             align_params = {'ndex_uuid' : None,
                             'ndex_server' : 'http://public.ndexbio.org/',
                             'name' : 'Data-Driven Ontology',
                             'ont1_ndex_uuid' : None,
                             'ont2_ndex_uuid' : None,
+                            'calculateFDRs_cmd': os.path.join(os.getenv('ALIGN_ONTOLOGY'), 'calculateFDRs'),
+                            'iterations' : 3,
+                            'threads' : 4,
                             'verbose': True}
 
             input_G, align_params, errors = self.read_element_stream(element_iterator, align_params)
+
+            print 'Parameters:'
+            print align_params
 
             ###############################
 
             if isinstance(align_params['ont1_ndex_uuid'], (str, unicode)) and \
                isinstance(align_params['ont2_ndex_uuid'], (str, unicode)):
-                # Read graph using NDEx client
+                ## Read graph using NDEx client
+
+                print 'Reading hierarchy 1'
                 hier1 = NdexGraph(server=align_params['ndex_server'],
                                   username=align_params['ndex_user'],
                                   password=align_params['ndex_pass'],
                                   uuid=align_params['ont1_ndex_uuid'])
 
+                print 'Reading hierarchy 2'
                 hier2 = NdexGraph(server=align_params['ndex_server'],
                                   username=align_params['ndex_user'],
                                   password=align_params['ndex_pass'],
-                                  uuid=align_params['ont2_ndex_uuid'])                
+                                  uuid=align_params['ont2_ndex_uuid'])
             else:
                 raise Exception()
 
-            if len(errors) == 0:               
-                graph = 
+#            if len(errors) == 0:
+            if True:
+                print 'Creating list representation of hierarchies'
+                tr = lambda x : 'gene' if x=='Gene-Term Annotation' else 'default'
 
-                hier1_edges = [(hier1.node[u]['name'], hier1.node[v]['name'], attr['EdgeType']) for u, v, attr in hier1.edges_iter(data=True)]
-                hier2_edges = [(hier2.node[u]['name'], hier2.node[v]['name'], attr['EdgeType']) for u, v, attr in hier2.edges_iter(data=True)]
-                align_hierarchies(hier1, hier2,
-                                  align_params['output'],
-                                  align_params['iterations'],
-                                  align_params['threads'])
+                hier1_ont = NdexGraph_to_Ontology(hier1, gene_term='Gene-Term Annotation')
+                hier2_ont = NdexGraph_to_Ontology(hier2, gene_term='Gene-Term Annotation')
+                print 'Summary of hier1_ont:'
+                print hier1_ont.summary()
+                print 'Summary of hier2_ont:'
+                print hier2_ont.summary()
+
+                common_genes = set(hier1_ont.genes) & set(hier2_ont.genes)
+
+                hier1_ont.delete_genes(set(hier1_ont.genes) - common_genes)
+                hier1_collapsed = hier1_ont.collapse_ontology(method='mhkramer')
+                print 'Summary of hier1_collapsed:'
+                print hier1_collapsed.summary()
+
+                hier2_ont.delete_genes(set(hier2_ont.genes) - common_genes)
+                hier2_collapsed = hier2_ont.collapse_ontology(method='mhkramer')
+                print 'Summary of hier2_collapsed:'
+                print hier2_collapsed.summary()
+
+                assert len(hier1_collapsed.terms) < 3000, len(hier1_collapsed.terms)
+                assert len(hier2_collapsed.terms) < 3000, len(hier2_collapsed.terms)
+
+                hier1_collapsed_nx = hier1_collapsed.to_networkx()
+                hier2_collapsed_nx = hier2_collapsed.to_networkx()
                 
-                alignment = pd.read_csv(align_params['output'],
-                                        columns=['Term_1', 'Term_2', 'Similarity', 'FDR', 'Term_1_Size'],
-                                        header=None,
-                                        sep='\t')
-                for t in hier1.nodes:
-                    hier1.node[t]['Aligned_Term'] = alignment[t]['Term_2']
-                    hier1.node[t]['Aligned_Similarity'] = alignment[t]['Term_2']
-                    hier1.node[t]['Aligned_FDR'] = alignment[t]['Term_2']
-                #yield hier1
+                def nx_to_edges(G):
+                    return [(G.node[v]['name'], G.node[u]['name'], tr(attr['EdgeType'])) \
+                               for u, v, attr in G.edges_iter(data=True)]
 
+                hier1_edges = nx_to_edges(hier1_collapsed_nx)
+                hier2_edges = nx_to_edges(hier2_collapsed_nx)
+                
+#                with tempfile.NamedTemporaryFile('w', delete=True) as output_file:
+                with tempfile.NamedTemporaryFile('w', delete=False) as output_file:
+                    output = output_file.name
+                    print 'output:', output
+
+                    print 'Aligning hierarchies'
+                    align_hierarchies(hier1_edges, hier2_edges,
+                                      output,
+                                      align_params['iterations'],
+                                      align_params['threads'])
+                
+                    alignment = pd.read_csv(output,
+                                            names=['Term_1', 'Term_2', 'Similarity', 'FDR', 'Term_1_Size'],
+                                            header=None,
+                                            sep='\t')
+
+                # import cPickle
+                # f = open('/tmp/tmp.pickle', 'wb')
+                # print 'Writing pickle'
+                # cPickle.dump({'alignment':alignment, 'hier1':hier1}, f)
+                # f.close()
+
+                # hier1_names = nx.get_node_attributes(hier1, 'name')
+                # assert len(set(hier1_names.values())) == len(hier1_names.values())
+                # hier1_names_idx = {b : a for a, b in hier1_names.items()}
+                # hier1_nodes = hier1.nodes(data=True)
+
+                print 'One-to-one term alignments:', alignment.shape[0]
+                print alignment.iloc[:30,:]
+
+#                G.get_node_ids("Brown","Color")
+#                term1_ids = G.get_node_ids(*alignment['Term_1'])
+
+                # for col in ['Term_2', 'Similarity', 'FDR']:
+                #     nx.set_node_attributes(hier1, col, {hier1_names_idx[t1] : v for t1, v in zip(alignment['Term_1'], alignment[col])})
+
+                for index, row in alignment.iterrows():
+#                    term1 = hier1_nodes[hier1_names_idx[row['Term_1']]][1]
+                    term1 = hier1.node[hier1.get_node_ids(row['Term_1'])[0]]
+                    print 'Term1:', term1
+                    term1['Aligned_Term'] = row['Term_2']
+                    term1['Aligned_Similarity'] = row['Similarity']
+                    term1['Aligned_FDR'] = row['FDR']
+
+                ndex_url = hier1.upload_to(align_params['ndex_server'], align_params['ndex_user'], align_params['ndex_pass'])
+                ndex_uuid = ndex_url.split('v2/network/')[1]
+                print 'ndex_uuid:', ndex_uuid
+
+                element = cx_pb2.Element()
+                netAttr = element.networkAttribute
+                netAttr.name = 'ndex_uuid'
+                netAttr.value = ndex_uuid
+                yield element
             else:
                 for caught_error in errors:
                     error = self.create_internal_crash_error(caught_error.message, 500)

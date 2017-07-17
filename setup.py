4b4f82c8f4d59ce7ddb69b20622006a70bcf5236
Michael Ku Yu <mikeyu.ucsd.edu@gmail.com>
2017-07-17 14:07:01 -0700

    Added miscellaneous config files

---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+#, read_configuration
+
+# import ConfigParser
+# Config = ConfigParser.ConfigParser()
+# Config.read('ddot/site.cfg')
+# print 'SECTIONS', Config.sections()
+# print Config.items('executables')
+
+#conf_dict = read_configuration('ddot/setup.cfg')
+#print conf_dict
+
+#import ddot.config
+
+# with open('tmp.py', 'w') as f:
+#     f.write('x = 5\n')
+
+setup(name='ddot',
+      version='0.1',
+      description='Data-Driven Ontology Toolkit',
+      url='http://github.com/michaelkyu/ontology_cx',
+      author='Michael Ku Yu',
+      author_email='michaelyu@alum.mit.edu',
+      license='MIT',
+      classifiers=[
+          # How mature is this project? Common values are 
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: MIT License',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2.7'
+      ],
+      keywords='ontology hierarchy',
+      packages=['ddot', 'ddot.cx_services'],
+      install_requires=['pandas', 'numpy', 'python-igraph', 'networkx'],
+      include_package_data=True,
+      zip_safe=False)
+
+      # package_data={
+      #     '':['config.py', 'site.cfg'],
+      # },
+

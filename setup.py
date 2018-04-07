01d0cfc6ffdfb026bb96736320b1b7ec25ee8285
Michael Ku Yu <mikeyu.ucsd.edu@gmail.com>
2018-04-06 19:21:10 -0700

    Removed requests package as a dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ddot',
-      version='0.1',
+      version='0.2dev1',
       description='Data-Driven Ontology Toolkit',
       url='http://github.com/michaelkyu/ontology',
       author='Michael Ku Yu',
@@ -16,18 +16,22 @@ setup(name='ddot',
 
           # Indicate who your project is intended for
           'Intended Audience :: Developers',
-          'Topic :: Software Development :: Build Tools',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Scientific/Engineering :: Visualization',
 
           # Pick your license as you wish (should match "license" above)
           'License :: OSI Approved :: MIT License',
 
           # Specify the Python versions you support here. In particular, ensure
           # that you indicate whether you support Python 2, Python 3 or both.
-          'Programming Language :: Python :: 2.7'
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.5'
       ],
       keywords='ontology hierarchy',
       packages=['ddot'],
-      install_requires=['pandas>=0.20', 'numpy', 'scipy', 'ndex-dev', 'python-igraph', 'networkx==1.11', 'requests', 'tulip-python'],
+      install_requires=['pandas>=0.20', 'numpy', 'scipy', 'ndex-dev', 'python-igraph', 'networkx==1.11', 'tulip-python'],
       include_package_data=True,
       zip_safe=False)
 
